f0e69345fac2bcffc683540eee59ab69194534e5
Amarjeet Soni <amarjeetsoni27@gmail.com>
2024-01-27 18:13:30 +0530

    backend - Initial Commit

---
+++ src/main/java/com/ExamGuruOnline/ExamGuruOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.ExamGuruOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGuruOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamGuruOnlineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ExamGuruOnline/test/TestController.java
@@ -0,0 +1,28 @@
+package com.ExamGuruOnline.test;
+
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "test")
+@CrossOrigin("*")
+public class TestController {
+
+	@GetMapping(value = "/getName")
+	public ResponseEntity<Object> getNameString() {
+		return new ResponseEntity<>(Arrays.asList("Abhay", "Abhi", "Ati", "Ashi", "Ashu", "Amar", "Abhinav"), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/getHello")
+	public ResponseEntity<Object> getHelloWord() {
+		return new ResponseEntity<>("Hello World!!", HttpStatus.OK);
+	}
+	
+}

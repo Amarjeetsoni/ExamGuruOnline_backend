a97cf8d92fa095ef1c3eb3414cf8c20b7f7bbeaf
Amarjeet Soni <amarjeetsoni27@gmail.com>
2024-04-06 18:22:56 +0530

    Added Cross Origin

--- src/main/java/com/ExamGuruOnline/controller/CategoryController.java
+++ src/main/java/com/ExamGuruOnline/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.ExamGuruOnline.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ExamGuruOnline.serviceInterface.CategoryServiceInterface;
 
 @RestController
+@CrossOrigin("*")
 public class CategoryController {
 
 	@Autowired
